b410ab40b90b531712c449dc06bb68c3f97e88af
Deepak <deepakdesai19@gmail.com>
2017-09-03 16:28:17 +0530

    srver

--- server.js
+++ server.js
@@ -61,7 +61,10 @@ app.post('/email', function(req, res){
 //   console.log('listening on *:'+app.get('port'));
 // });
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+    console.log("App is running on port " + port);
 });
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
