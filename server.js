46c64bf04d3dbfe981d3b3db13d3b1ae66b177a1
Deepak <deepakdesai19@gmail.com>
2017-09-03 14:30:39 +0530

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 var ex = require('express');
+var nodemailer = require('nodemailer');
 var app=ex();
 var http = require('http').Server(app);
 var bParser = require('body-parser');
@@ -21,6 +22,40 @@ app.use(bParser.urlencoded({extended:false}));
 app.get('/',function(req,res){
 	res.render('index');
 	});
+
+app.get('/contact',function(req,res){
+	res.render('contact');
+	});
+
+
+app.post('/email', function(req, res){
+
+
+	var transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+	    user: 'deepakdesai0906@gmail.com',
+	    pass: 'Cocubesdeepak'
+	  }
+	});
+
+	var mailOptions = {
+	  from: 'deepakdesai0906@gmail.com',
+	  to: "jadhav.karan123@gmail.com",
+	  subject: 'Picagroexporter',
+	  html: '<h1 style="text-align:center"><a href="picagroexporter.com">Picagroexporter.com</a></h1><br><h2>Ordered By '+req.body.yrname+'</h2><br>Name: <b>'+req.body.yrname+'</b><br>Contact No: <a href="tel:'+req.body.conno+'"><b>'+req.body.conno+'</b></a><br>Email Id: <b>'+req.body.email+'</b><br>Order Description: <b>'+req.body.desc+'</b><br>Order Quantity: <b>'+req.body.quantity+' '+req.body.size+'</b><br>Type: <b>'+req.body.type+'</b><br>Destination Port: <b>'+req.body.port+'</b><br>Shipping By: <b>'+req.body.shipby
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	});
+
+	res.redirect('/contact');
+});
 
 
 http.listen(app.get('port'), function(){
