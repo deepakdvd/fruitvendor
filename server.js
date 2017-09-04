ac6cda8d5ca70a7199fdd6b53516eb9eda59a025
Deepak <deepakdesai19@gmail.com>
2017-09-04 21:19:42 +0530

    email2

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ console.log("jhj");
    host: 'smtp.zoho.com',
     port: 465,
     secure: true, 
-    service: 'gmail',
+    service: 'Gmail',
   auth: {
     user: 'deepakdesai0906@gmail.com',
     pass: 'Cocubesdeepak'
@@ -87,7 +87,7 @@ transporter.sendMail(mailOptions, function(error, info){
   }
 });
 
-res.redirect('/contact');
+
 
 });
 
