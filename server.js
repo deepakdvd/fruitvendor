a6cc385faf9a00c232535add1bd4750eb1116dd2
Deepak <deepakdesai19@gmail.com>
2017-09-05 00:04:06 +0530

    about page

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.get('/contact',function(req,res){
 	res.render('contact');
 	});
 
+app.get('/about',function(req,res){
+	res.render('about');
+	});
+
 // app.get('/email', function(req, res){
 
 
