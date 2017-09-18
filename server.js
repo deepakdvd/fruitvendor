cf725b7c3f56f638eff87748bd35ca3a83d00752
Deepak <deepakdesai19@gmail.com>
2017-09-18 21:31:18 +0530

    zoho final

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/email',function(req, res){
 
 var mailOptions = {
   from: 'deepakdesai19@picagroexporter.com',
-  to: 'deepakdesai19@gmail.com',
+  to: 'jadhav.karan123@gmail.com',
   subject: 'Enquiry/Order',
   html: '<h1 style="text-align:center"><a href="picagroexporter.com">Picagroexporter.com</a></h1><br><h2>Ordered By '+req.body.yrname+'</h2><br>Name: <b>'+req.body.yrname+'</b><br>Contact No: <a href="tel:'+req.body.conno+'"><b>'+req.body.conno+'</b></a><br>Email Id: <b>'+req.body.email+'</b><br>Order Description: <b>'+req.body.desc+'</b><br>Order Quantity: <b>'+req.body.quantity+' '+req.body.size+'</b><br>Type: <b>'+req.body.type+'</b><br>Destination Port: <b>'+req.body.port+'</b><br>Shipping By: <b>'+req.body.shipby
 };
